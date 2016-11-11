b1ac4251cc44294a5be3254d59ff8c51bf9f8f68
root <joao_nad@hotmail.com>
2016-11-11 11:00:44 -0200

    Cadastro e exibicao de treinos funcionando.

--- app/src/main/java/br/com/newkeepinshape/activity/exercicio/ExericioLista.java
+++ app/src/main/java/br/com/newkeepinshape/activity/exercicio/ExericioLista.java
@@ -1,22 +1,17 @@
 package br.com.newkeepinshape.activity.exercicio;
 
 import android.app.ListActivity;
-import android.database.Cursor;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ArrayAdapter;
+
 import android.widget.ListAdapter;
+
 import android.widget.SimpleAdapter;
-import android.widget.SimpleCursorAdapter;
+
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -33,13 +28,16 @@ public class ExericioLista extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_exericio_lista);
+        setContentView(R.layout.activity_exericio_lista);
 
         try {
+
             exercicioService = ExercicioService.valueOfExercicioService(new ExercicioDaoIml(this));
-            final ListAdapter adapter = new SimpleAdapter(this, getExercicios(), R.id.row_exercicio_layout,
+            final ListAdapter adapter = new SimpleAdapter(this, getExercicios(), R.layout.row_exercicio_layout,
                     new String[] {"Nome", "Peso"}, new int [] {R.id.exercicoNome, R.id.exercicioPeso});
+
             setListAdapter(adapter);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
